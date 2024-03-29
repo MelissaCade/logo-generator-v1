2d0d6bf69de4eb9ebad3478fde8747498313e142
BookWyrmMom <160056156+BookWyrmMom@users.noreply.github.com>
2024-03-28 21:19:04 -0600

    added shape and text modules

--- index.js
+++ index.js
@@ -1,65 +1,65 @@
 import inquirer from "inquirer";
 import jest from "jest";
 import fs from "fs";
-// import { userInput as userData } from "./userInput.js";
 
-let userInput = {};
+const questions = [
+  {
+    type: "input",
+    message: "Please enter up to three letters for your logo.",
+    name: "textContent",
+  },
+  {
+    type: "input",
+    message: "Please enter a color name or hexadecimal code for your text.",
+    name: "textColor",
+  },
+  {
+    type: "list",
+    message: "Please choose a font for your text.",
+    name: "textFont",
+    choices: [
+      "Arial",
+      "Brush Script MT",
+      "Courier New",
+      "Garamond",
+      "Georgia",
+      "Tahoma",
+      "Times New Roman",
+      "Trebuchet MS",
+      "Verdana",
+    ],
+  },
+  {
+    type: "list",
+    message: "Please choose a background shape.",
+    name: "backgroundShape",
+    choices: ["square", "circle", "ellipse", "triangle", "hexagon"],
+  },
+  {
+    type: "input",
+    message:
+      "Please enter a color name or hexadecimal code for your background shape.",
+    name: "backgroundColor",
+  },
+  {
+    type: "input",
+    message: "Please enter a file name for your new svg logo.",
+    name: "fileName",
+  },
+];
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "Please enter up to three letters for your logo.",
-      name: "textContent",
-    },
-    {
-      type: "input",
-      message: "Please enter a color name or hexadecimal code for your text.",
-      name: "textColor",
-    },
-    // {
-    //   type: "list",
-    //   message: "Please choose a font for your text.",
-    //   name: "textFont",
-    //   choices: [
-    //     "Arial",
-    //     "Brush Script MT",
-    //     "Courier New",
-    //     "Garamond",
-    //     "Georgia",
-    //     "Tahoma",
-    //     "Times New Roman",
-    //     "Trebuchet MS",
-    //     "Verdana",
-    //   ],
-    // },
-    {
-      type: "list",
-      message: "Please choose a background shape.",
-      name: "backgroundShape",
-      choices: ["square", "circle", "ellipse", "triangle", "hexagon"],
-    },
-    {
-      type: "input",
-      message:
-        "Please enter a color name or hexadecimal code for your background shape.",
-      name: "backgroundColor",
-    },
-    {
-      type: "input",
-      message: "Please enter a file name for your new svg logo.",
-      name: "fileName",
-    },
-  ])
-  .then((answers) => {
-    userInput = answers;
+function start() {
+  inquirer.prompt(questions).then((answers) => {
     fs.writeFile(
       "userInput.json",
-      JSON.stringify({ userInput }, null, "\t"),
+      JSON.stringify({ answers }, null, "\t"),
       (err) => {
         err
           ? console.log(err)
-          : console.log("User Data succesfully saved to file.");
+          : console.log("User Data successfully saved to file.");
       }
     );
   });
+}
+
+start();
--- library/shapes.js
+++ library/shapes.js
@@ -1,3 +1,26 @@
-import questions from "../index.js";
+import fs from "fs";
+const input = "../userinput.json";
 
-questions();
+const generateShape = () =>
+  fs.readFile(input, "utf8", (err, data) => {
+    const userData = JSON.parse(data);
+    const shape = userData.answers.backgroundShape;
+    if (shape === "square") {
+      const svgShape = `<rect width="300" height="300" fill="${userData.answers.backgroundColor}" />`;
+      return svgShape;
+    } else if (shape === "circle") {
+      const svgShape = `<circle r="150" cx="150" cy="150" fill="${userData.answers.backgroundColor}" />`;
+      return svgShape;
+    } else if (shape === "ellipse") {
+      const svgShape = `<ellipse rx="150" ry="100" cx="150" cy="150" fill="${userData.answers.backgroundColor}" />`;
+      return svgShape;
+    } else if (shape === "triangle") {
+      const svgShape = `<polygon points="150 20.1 300 279.9 0 279.9" fill="${userData.answers.backgroundColor}" />`;
+      return svgShape;
+    } else if (shape === "hexagon") {
+      const svgShape = `<polygon points="75 20.1 225 20.1 300 150 225 279.9 75 279.9 0 150" fill="${userData.answers.backgroundColor}" />`;
+      return svgShape;
+    }
+  });
+generateShape();
+// module.exports = generateShape;
